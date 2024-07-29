f19984ab2780484647415819d1ebf2e64d645352
Bob Parsons <deadstylebp@gmail.com>
2024-07-29 08:35:28 -0500

    Update kitty files

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -3,6 +3,32 @@ vim.cmd("let g:netrw_liststyle = 3")
 
 local opt = vim.opt
 
+function Get_git_project_and_file()
+	local file = vim.fn.expand("%:t")
+	if file == "" then
+		file = "[No Name]"
+	end
+
+	local file_icon = "📄"
+
+	local git_dir = vim.fn
+		.system("git -C " .. vim.fn.shellescape(vim.fn.expand("%:p:h")) .. " rev-parse --show-toplevel 2>/dev/null")
+		:gsub("\n", "")
+
+	if vim.v.shell_error ~= 0 then
+		return file_icon .. " " .. file
+	else
+		local dir_icon = "📁"
+		return file_icon .. " " .. file .. " | " .. dir_icon .. " " .. vim.fn.fnamemodify(git_dir, ":t")
+	end
+end
+
+vim.opt.title = true
+vim.opt.titlestring = "%{v:lua.Get_git_project_and_file()}"
+
+vim.opt.title = true
+vim.opt.titlestring = "%{v:lua.Get_git_project_and_file()}"
+
 -- Lines
 opt.relativenumber = true
 opt.number = true

67bfbf2aa7353c968d0153d40735b4edf5bd0f3f
Bob Parsons <deadstylep@gmail.com>
2025-01-19 20:36:49 -0600

    setup neotree

--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -3,13 +3,12 @@ return {
 	branch = "v3.x",
 	dependencies = {
 		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", 
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
 		"MunifTanjim/nui.nvim",
-		"3rd/image.nvim",
+		"3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
 	},
 	config = function()
 		require("neo-tree").setup({})
 	end,
-
 	addToKeyMap("n", "<leader>e", "<cmd>Neotree reveal position=float<CR>", "Toggle file explorer on current file"),
 }

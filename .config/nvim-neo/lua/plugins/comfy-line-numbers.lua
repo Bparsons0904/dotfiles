2b9817962c2a478ead451e04c8d1875285fee3fc
Bob Parsons <deadstylebp@gmail.com>
2025-02-11 17:40:43 -0600

    remove line numbers from telescope

--- .config/nvim-neo/lua/plugins/comfy-line-numbers.lua
+++ .config/nvim-neo/lua/plugins/comfy-line-numbers.lua
@@ -2,6 +2,21 @@ return {
   "mluders/comfy-line-numbers.nvim",
   enabled = true,
   config = function()
-    require("comfy-line-numbers").setup()
+    require("comfy-line-numbers").setup({
+      hidden_buffer_types = {
+        "terminal",
+        "telescope",
+        "prompt",
+        "popup",
+        "nofile",
+      },
+      hidden_file_types = {
+        "TelescopeResults",
+        "TelescopePrompt",
+        "TelescopePreview",
+        "telescope",
+        "Telescope",
+      },
+    })
   end,
 }

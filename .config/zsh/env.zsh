7d4051323de68b2f4e4e88ce3a667dff2b70bc1b
Bob Parsons <deadstylep@gmail.com>
2025-03-07 19:46:40 -0600

    update nvim nvim path

--- .config/zsh/env.zsh
+++ .config/zsh/env.zsh
@@ -1,3 +1,5 @@
-export PATH="$PATH:$HOME/.local/bin"
-export PATH="$PATH:$HOME/.local/bin:/opt/nvim-linux64/bin"
+# export PATH="$PATH:$HOME/.local/bin"
+# export PATH="$PATH:$HOME/.local/bin:/opt/nvim-linux64/bin"
+# export EDITOR=nvim
+export PATH="/opt/nvim-linux64/bin:$HOME/.local/bin:$PATH"
 export EDITOR=nvim

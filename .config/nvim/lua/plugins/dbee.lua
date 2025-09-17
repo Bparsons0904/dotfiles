c04049c906adc63c82a6bcebd4c4da2c5edfd124
Bob Parsons <deadstylebp@gmail.com>
2025-09-17 08:50:04 -0500

    dbee add

--- .config/nvim/lua/plugins/dbee.lua
+++ .config/nvim/lua/plugins/dbee.lua
@@ -7,6 +7,16 @@ return {
     require("dbee").install("go")
   end,
   config = function()
-    require("dbee").setup()
+    require("dbee").setup({
+      sources = {
+        require("dbee.sources").MemorySource:new({
+          {
+            name = "Waugze Dev",
+            type = "postgres",
+            url = 'postgres://waugzee_dev_user:{{ env "DB_PASSWORD" }}@192.168.86.203:5432/waugzee_dev?sslmode=disable',
+          },
+        }),
+      },
+    })
   end,
 }

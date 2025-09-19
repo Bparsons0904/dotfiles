299781a393e50f241c0d089076966541d78c8b83
Bob Parsons <deadstylebp@gmail.com>
2025-09-18 21:54:14 -0500

    setup dbee keybindings

--- .config/nvim/lua/plugins/dbee.lua
+++ .config/nvim/lua/plugins/dbee.lua
@@ -9,14 +9,27 @@ return {
   config = function()
     require("dbee").setup({
       sources = {
-        require("dbee.sources").MemorySource:new({
-          {
-            name = "Waugze Dev",
-            type = "postgres",
-            url = 'postgres://waugzee_dev_user:{{ env "DB_PASSWORD" }}@192.168.86.203:5432/waugzee_dev?sslmode=disable',
-          },
-        }),
+        require("dbee.sources").EnvSource:new("DBEE_CONNECTIONS"),
       },
     })
   end,
+
+  addKeyMaps({
+    {
+      "n",
+      "<leader>do",
+      function()
+        require("dbee").open()
+      end,
+      "Open dbee",
+    },
+    {
+      "n",
+      "<leader>dc",
+      function()
+        require("dbee").close()
+      end,
+      "Close dbee",
+    },
+  }),
 }

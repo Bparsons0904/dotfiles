f13d4fa7dae07d261a75ffbc43b2e447e13d20df
Bob Parsons <deadstylebp@gmail.com>
2025-10-10 20:26:39 -0500

    add oasis theme

---
+++ .config/nvim/lua/colorschemes/oasis.lua
@@ -0,0 +1,23 @@
+return {
+  "uhs-robert/oasis.nvim",
+  lazy = false,
+  priority = 1000,
+  config = function()
+    require("oasis").setup({
+      style = "abyss",
+    })
+  end,
+}
+-- colorscheme oasis-abyss
+-- colorscheme oasis-cactus
+-- colorscheme oasis-canyon
+-- colorscheme oasis-desert
+-- colorscheme oasis-dune
+-- colorscheme oasis-lagoon
+-- colorscheme oasis-mirage
+-- colorscheme oasis-night
+-- colorscheme oasis-midnight
+-- colorscheme oasis-rose
+-- colorscheme oasis-sol
+-- colorscheme oasis-starlight
+-- colorscheme oasis-twilight

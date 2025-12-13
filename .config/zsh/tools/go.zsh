0af7d31fb157c928f31723463eaad6216d1ad479
Bob Parsons <deadstylebp@gmail.com>
2025-12-12 20:46:00 -0600

    update go path

--- .config/zsh/tools/go.zsh
+++ .config/zsh/tools/go.zsh
@@ -1,3 +1,4 @@
 export GOPATH="$HOME/go"
 export GOBIN="$GOPATH/bin"
-export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
+# export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin"
+export PATH="$PATH:/usr/local/go/bin:$GOPATH/bin:/home/bobparsons/Development/dingo"
